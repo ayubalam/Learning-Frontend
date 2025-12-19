c48181a602e11984f6a2e6902040867af95ba58f
ayubalam <kaayub706@gmail.com>
2025-12-19 10:27:51 +0530

    url

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://learning-backend-1-zpec.onrender.com/api",
 });
 
 export default API;
